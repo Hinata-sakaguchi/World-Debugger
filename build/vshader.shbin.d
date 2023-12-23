a4daeee18bd1443862b4c68b0d49e3ff30a3153e
Hinata-sakaguchi <netawakuryusei@gmail.com>
2023-12-23 19:01:32 +0900

    fix 2d and 3d

---
+++ build/vshader.shbin.d
@@ -0,0 +1,1 @@
+vshader.shbin.o: /home/ryuai/Videos/World-Debugger/source/vshader.v.pica /home/ryuai/Videos/World-Debugger/source/vshader.v.pica
---
+++ include/all_vertex.h
@@ -0,0 +1,10 @@
+#ifndef ALL_VERTX_H
+#define ALL_VERTX_H
+
+#include "Vertex/base_vertex.h"
+#include "Vertex/people_vertex.h"
+#include "Vertex/road_vertex.h"
+#include "Vertex/stop_vertex.h"
+#include "Vertex/tunnel_vertex.h"
+
+#endif
